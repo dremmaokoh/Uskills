401ea5452faec098f05ad888ec1f696448960f16
Pipemma <dremmaokoh@gmail.com>
2022-10-16 22:58:55 +0100

    make it better

--- config/db.config.js
+++ config/db.config.js
@@ -2,6 +2,7 @@ module.exports = {
     HOST: process.env.POSTGRESQL_DB_HOST,
     USER: process.env.POSTGRESQL_DB_USER,
     PASSWORD: process.env.POSTGRESQL_DB_PASSWORD,
+    PORT: process.env.POSTGRESQL_DB_PORT,
     DB: process.env.POSTGRESQL_DB,
 
     // logging : false,
